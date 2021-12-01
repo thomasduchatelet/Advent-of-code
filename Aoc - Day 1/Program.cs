eadffb75da59e2493015f5f46ade41bfdbe1ed21
Thomas Duchatelet <tduchatelet@inimco.com>
2021-12-01 09:30:49 +0100

    Add project files.

---
+++ Aoc - Day 1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Aoc___Day_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] lines = System.IO.File.ReadAllLines(@"input\input.txt");
+
+            var amount = 0;
+
+            for (int i = 1; i < lines.Length; i++){
+                if (double.Parse(lines[i]) > double.Parse(lines[i - 1]))
+                    amount++;
+            }
+
+            Console.WriteLine("Answer part 1: " + amount);
+
+            amount = 0;
+
+            for (int i = 0; i < lines.Length - 3; i++)
+            {
+                var sum1 = double.Parse(lines[i]) + double.Parse(lines[i + 1]) + double.Parse(lines[i + 2]);
+                var sum2 = double.Parse(lines[i + 1]) + double.Parse(lines[i + 2]) + double.Parse(lines[i + 3]);
+                if (sum2 > sum1)
+                    amount++;
+            }
+
+            Console.WriteLine("Answer part 2: " + amount);
+        }
+    }
+}
